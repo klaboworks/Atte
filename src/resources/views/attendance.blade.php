ba8e3cff6044298b95c2cfecaef674e14cdeb06c
KenTanaka <klaboworks@gmail.com>
2024-08-27 12:18:21 +0900

    勤務時間と休憩時間取得の仮メソッド作成 #3

--- src/resources/views/attendance.blade.php
+++ src/resources/views/attendance.blade.php
@@ -34,22 +34,24 @@
                     <th class="user-name">{{$user->name}}</th>
                     <td>
                         @foreach($works as $work)
-                        @if($user->id === $work->users_id)
+                        @if($user->id === $work->user_id)
                         {{$work->start_work}}
                         @endif
                         @endforeach
                     </td>
                     <td>
                         @foreach($works as $work)
-                        @if($user->id === $work->users_id)
+                        @if($user->id === $work->user_id)
                         {{$work->end_work}}
                         @endif
                         @endforeach
                     </td>
                     <td>
-                      
+                        {{ $workTimes }}
                     </td>
-                    <td>09:30:00</td>
+                    <td>
+                        {{ $restSums }}
+                    </td>
                 </tr>
                 @endforeach
             </tbody>
