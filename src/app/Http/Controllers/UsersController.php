fcfded2ffc1e267ace90ab1fd9b7b380edb2d27b
KenTanaka <klaboworks@gmail.com>
2024-09-25 23:09:12 +0900

    ユーザーごとの勤怠一覧作成 #28

--- src/app/Http/Controllers/UsersController.php
+++ src/app/Http/Controllers/UsersController.php
@@ -4,18 +4,28 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Carbon\Carbon;
+use Carbon\CarbonPeriod;
 
 class UsersController extends Controller
 {
     public function index()
     {
         $users = User::all();
-        return view('user.index',compact('users'));
+
+        return view('user.index', compact('users'));
     }
 
     public function showUserAttendance(Request $request)
     {
         $user = User::find($request->id);
-        return view('user.user_attendance', compact('user'));
+        $month = new Carbon($request->month);
+        $period = CarbonPeriod::create(now()->startOfMonth(), now()->endOfMonth());
+
+        foreach ($period as $months) {
+            $dates[] = $months;
+        }
+
+        return view('user.user_attendance', compact('user', 'month', 'dates'));
     }
 }
